ae2b58df7c3ee3adedaab6329a82795268938d69
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-06 14:43:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,13 +42,13 @@ def apply_clearance(cart)
 end 
 
 def checkout(items, coupons)
-  cart = consolidate_cart(items)
-  cart1 = apply_coupons(cart, coupons)
-  cart2 = apply_clearance(cart1)
+  cart1 = consolidate_cart(items)
+  cart2 = apply_coupons(cart, coupons)
+  cart3 = apply_clearance(cart1)
   
   total = 0
   
-  cart2.each do |name, price_hash|
+  cart3.each do |name, price_hash|
     total += price_hash[:price] * price_hash[:count]
   end
   
